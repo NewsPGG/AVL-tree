177a4aa5f2d92cd59a7097d3bd330d932febc0f9
Кирилл Симонов <kirillsimonov3@mail.ru>
2026-03-22 21:48:07 +0300

    Tests to AVL Tree

--- Tests/MainTask/AVL-Tree/TestAVL-Tree.c
+++ Tests/MainTask/AVL-Tree/TestAVL-Tree.c
@@ -2,7 +2,8 @@
 #include <assert.h>
 #include <stdlib.h>
 
-static int checkAVLTree(AVLNode* node) {
+static int checkAVLTree(AVLNode* node)
+{
     if (node == NULL) {
         return -1;
     }
@@ -28,32 +29,36 @@ static int checkAVLTree(AVLNode* node) {
     return realHeight;
 }
 
-static int isAVLTree(AVLTree* tree) {
+static int isAVLTree(AVLTree* tree)
+{
     return checkAVLTree(tree->root) != -1;
 }
 
-void TestCreateAVLTree() {
+void TestCreateAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
     assert(tree->root);
     FreeAVLTree(tree);
 }
 
-void TestInsertOneElementAVLTree() {
+void TestInsertOneElementAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
 
-    AVLTreeInsert(tree, "ABC");
+    AVLTreeInsert(tree, "ABC", "Hello World");
     assert(AVLTreeFind(tree, "ABC"));
 
     FreeAVLTree(tree);
 }
 
-void TestDeleteOneElementAVLTree() {
+void TestDeleteOneElementAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
 
-    AVLTreeInsert(tree, "ABC");
+    AVLTreeInsert(tree, "ABC", "Hello World");
     assert(AVLTreeFind(tree, "ABC"));
     AVLTreeDelete(tree, "ABC");
     assert(!AVLTreeFind(tree, "ABC"));
@@ -61,26 +66,28 @@ void TestDeleteOneElementAVLTree() {
     FreeAVLTree(tree);
 }
 
-void TestInsertTenElementsAVLTree() {
+void TestInsertTenElementsAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
 
     for (int i = 65; i < 76; i++) {
         char character = (char)i;
-        AVLTreeInsert(tree, &character);
+        AVLTreeInsert(tree, &character, &character);
         assert(AVLTreeFind(tree, &character));
     }
 
     FreeAVLTree(tree);
 }
 
-void TestDeleteTenElementAVLTree() {
+void TestDeleteTenElementAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
 
     for (int i = 65; i < 76; i++) {
         char character = (char)i;
-        AVLTreeInsert(tree, &character);
+        AVLTreeInsert(tree, &character, &character);
         assert(AVLTreeFind(tree, &character));
         AVLTreeDelete(tree, &character);
         assert(!AVLTreeFind(tree, &character));
@@ -89,52 +96,56 @@ void TestDeleteTenElementAVLTree() {
     FreeAVLTree(tree);
 }
 
-void TestLLBalanceAVLTree() {
+void TestLLBalanceAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
 
-    AVLTreeInsert(tree, "30");
-    AVLTreeInsert(tree, "20");
-    AVLTreeInsert(tree, "10");
+    AVLTreeInsert(tree, "30", "ABC");
+    AVLTreeInsert(tree, "20", "BCD");
+    AVLTreeInsert(tree, "10", "CDE");
 
     assert(isAVLTree(tree));
 
     FreeAVLTree(tree);
 }
 
-void TestLRBalanceAVLTree() {
+void TestLRBalanceAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
 
-    AVLTreeInsert(tree, "30");
-    AVLTreeInsert(tree, "10");
-    AVLTreeInsert(tree, "20");
+    AVLTreeInsert(tree, "30", "ABC");
+    AVLTreeInsert(tree, "10", "BCD");
+    AVLTreeInsert(tree, "20", "CDE");
 
     assert(isAVLTree(tree));
 
     FreeAVLTree(tree);
 }
 
-void TestRRBalanceAVLTree() {
+void TestRRBalanceAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
 
-    AVLTreeInsert(tree, "10");
-    AVLTreeInsert(tree, "20");
-    AVLTreeInsert(tree, "30");
+    AVLTreeInsert(tree, "10", "ABC");
+    AVLTreeInsert(tree, "20", "BCD");
+    AVLTreeInsert(tree, "30", "CDE");
 
     assert(isAVLTree(tree));
 
     FreeAVLTree(tree);
 }
 
-void TestRLBalanceAVLTree() {
+void TestRLBalanceAVLTree()
+{
     AVLTree* tree = CreateAVLTree();
     assert(tree);
 
-    AVLTreeInsert(tree, "10");
-    AVLTreeInsert(tree, "30");
-    AVLTreeInsert(tree, "20");
+    AVLTreeInsert(tree, "10", "ABC");
+    AVLTreeInsert(tree, "30", "BCD");
+    AVLTreeInsert(tree, "20", "CDE");
 
     assert(isAVLTree(tree));
 
